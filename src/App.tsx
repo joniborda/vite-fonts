fdbd09b7188b84f7779d3e6e03b8c048f871fd98
Jonathan Borda <jonathanmatiasborda@gmail.com>
2023-09-01 10:14:29 -0300

    change index

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,16 @@ export const App = () => {
       style={{
         flex: 1,
         width: "100%",
-        height: "100%",
+        height: "100vh",
         alignSelf: "stretch",
         display: "flex",
         position: "relative",
+        placeContent: "center",
+        alignItems: "center",
+        fontFamily: "sans-serif",
       }}
     >
+      Font sass is working fine
     </div>
   );
 };
