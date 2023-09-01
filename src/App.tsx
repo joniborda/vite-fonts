ddd2f6d6b5abb28b3fe07836b0387dafbf5c1477
Jonathan Borda <jonathanmatiasborda@gmail.com>
2023-09-01 09:34:03 -0300

    add hush fonts

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        flex: 1,
+        width: "100%",
+        height: "100%",
+        alignSelf: "stretch",
+        display: "flex",
+        position: "relative",
+      }}
+    >
+    </div>
+  );
+};
